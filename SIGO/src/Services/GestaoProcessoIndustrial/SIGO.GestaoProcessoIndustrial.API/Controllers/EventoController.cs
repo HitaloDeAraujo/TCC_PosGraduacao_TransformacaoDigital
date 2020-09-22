00edb768511b1b4b074956ddfff3e267c3f5f15a
Hitalo <hitalo.araujo@montreal.com.br>
2020-09-21 23:40:00 -0300

    Adicionando GestaoProcessoIndustrial

---
+++ SIGO/src/Services/GestaoProcessoIndustrial/SIGO.GestaoProcessoIndustrial.API/Controllers/EventoController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SIGO.GestaoProcessoIndustrial.Domain.Entidades;
+using System.Threading.Tasks;
+
+namespace SIGO.GestaoProcessoIndustrial.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EventoController : ControllerBase
+    {
+        [HttpGet]
+        //[Authorize]
+        public async Task<IActionResult> Get()
+        {
+            Evento evento = new Evento()
+            {
+                Nome = "Nome"
+            };
+
+            return Ok(evento);
+        }
+    }
+}
